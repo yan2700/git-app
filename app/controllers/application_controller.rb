3efe6f32e269ecc790f6e2c3999d67508f585e36
yan2700 <tomoyahyuga1213@gmail.com>
2025-05-12 19:44:58 +0900

    ログインログアウト編集ページ実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,13 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
+  def after_sign_up_path_for(resource)
+    root_path
+  end
+
+    def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'   # 例）postsコントローラーのindexアクションをトップページにする
   resources :posts
-  resources :users
+  resources :users, only: [:edit, :update]
 end
